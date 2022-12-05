b0092dffa361b12a063c3d2e089e07b59aa26438
Patrick Vigneault <pvigneault@cognibox.com>
2022-12-05 14:19:35 -0500

    Day 5

---
+++ day_5/Day-5-1.rb
@@ -0,0 +1,28 @@
+cargo = [
+  %w[M J C B F R L H],
+  %w[Z C D],
+  %w[H J F C N G W],
+  %w[P J D M T S B],
+  %w[N C D R J],
+  %w[W L D Q P J G Z],
+  %w[P Z T F R H],
+  %w[L V M G],
+  %w[C B G P F Q R J]
+]
+
+
+File.readlines('Day-5').each do |line|
+  count = line.split(' ')[1].to_i
+  from = line.split(' ')[3].to_i - 1
+  to = line.split(' ')[5].to_i - 1
+
+  cargo[to] = cargo[to] + cargo[from].pop(count).reverse
+end
+
+string = ''
+
+cargo.each do |stack|
+  string += stack.last
+end
+
+p string
